e5e2a3f5821fc624d2cbe8f19a871a8ab24fc15c
Genoris <117129255+Genoris@users.noreply.github.com>
2022-11-07 20:59:41 -0400

    Add files via upload

---
+++ Vehiculo20191149/Data/Models/Vehiculo.cs
@@ -0,0 +1,11 @@
+
+namespace Vehiculos20191149.Data.Models;
+
+public class Vehiculo 
+{
+    public int vehiculoID { get; set; }
+    public string? Marca { get; set; }
+    public string? Modelo  { get; set; }
+    public int Año  { get; set; }
+    public string? Color  { get; set; }
+}
